5f375732053d5afbec5840f603dd755d5c50daae
parshantsaini <sainiparshant1901@gmail.com>
2025-07-11 16:54:42 +0530

    error fixed

--- src/components/ui/moving-border.tsx
+++ src/components/ui/moving-border.tsx
@@ -80,9 +80,9 @@ export const MovingBorder = ({
   duration?: number;
   rx?: string;
   ry?: string;
-  [key: string]: any;
+  [key: string]: unknown;
 }) => {
-  const pathRef = useRef<any>();
+  const pathRef = useRef<SVGGeometryElement | null>(null);
   const progress = useMotionValue<number>(0);
 
   useAnimationFrame((time) => {
